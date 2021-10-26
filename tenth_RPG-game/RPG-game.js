30923e3ca8d7ea8bc3f9045c27962913abe1e917
dbd2h <kangminchang1@gmail.com>
2021-10-27 00:42:01 +0900

    RPG-game update

--- tenth_RPG-game/RPG-game.js
+++ tenth_RPG-game/RPG-game.js
@@ -2,7 +2,7 @@ const $startScreen = document.querySelector(".start-screen");
 const $gameMenu = document.querySelector(".game-menu");
 const $battleMenu = document.querySelector(".battle-menu");
 const $heroName = document.querySelector(".hero-inf__hero-stat");
-const $heroLavel = document.querySelector(".hero-inf__hero-level");
+const $heroLevel = document.querySelector(".hero-inf__hero-level");
 const $heroHp = document.querySelector(".hero-inf__hero-hp");
 const $heroXp = document.querySelector(".hero-inf__hero-xp");
 const $heroAtt = document.querySelector(".hero-inf__hero-att");
@@ -24,4 +24,31 @@ const monsterList = [
   { name: "스켈레톤", hp: 50, att: 15, xp: 20 },
   { name: "마왕", hp: 150, att: 35, xp: 50 },
 ];
-$startScreen.addEventListener("submit", (event) => {});
+function gameMenuSelect1() {
+  const randomNumber = Math.floor(Math.random() * 3);
+}
+function gameMenuSelect2() {}
+function gameMenuSelect3() {}
+
+$startScreen.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const name = event.target["start-screen__name-input"].value;
+  $startScreen.classList.toggle("hidden");
+  $gameMenu.classList.toggle("hidden");
+  $heroName.textContent = name;
+  $heroLevel.textcontent = `${hero.level}lev`;
+  $heroHp.textContent = `HP: ${hero.hp}/${hero.maxHp}`;
+  $heroXp.textContent = `XP: ${hero.xp}/${15 * hero.level}`;
+  $heroAtt.textContent = `ATT: ${hero.att}`;
+  hero.name = name;
+});
+
+$gameMenu.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const number = event.target["game-menu__input-number"].value;
+  if (number == 1) {
+  } else if (number == 2) {
+  } else if (number == 3) {
+  } else {
+  }
+});
